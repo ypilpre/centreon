d74f7ca554bfad963b15bc59614807747a82a4ae
Laurent Pinsivy <lpinsivy@centreon.com>
2019-03-05 14:41:25 +0000

    fix(update): remove non existing topology_JS entries

--- www/install/sql/centreon/Update-DB-18.10.3_to_18.10.4.sql
+++ www/install/sql/centreon/Update-DB-18.10.3_to_18.10.4.sql
@@ -3,3 +3,6 @@ UPDATE `informations` SET `value` = '18.10.4' WHERE CONVERT( `informations`.`key
 
 -- Add default illegal characters for centcore external commands
 INSERT INTO `options` (`key`, `value`) VALUES ('centcore_illegal_characters', '`');
+
+-- Remove non existing entries
+DELETE FROM topology_JS WHERE id_page IN ('201', '2020301', '2020302', '5010103', '5010105');
